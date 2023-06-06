2e108737afdb993ae3b41ebf3a25dbf63cd7e61e
WillBen33 <wilfrid.benoit@outlook.fr>
2023-06-06 14:30:39 +0200

    fix imports

--- src/main/java/com/springboot/jwt/auth/AuthController.java
+++ src/main/java/com/springboot/jwt/auth/AuthController.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("api/v1/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
